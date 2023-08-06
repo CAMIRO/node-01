e04e5afe3f7060182452bcac8b76917cbdc247d9
CAMIRO <juancamilosc@hotmail.com>
2023-08-06 07:27:05 -0500

    finishing pug files

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-const path = require('path')
+const path = require('path');
 
 const express = require('express');
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
 
 const app = express();
 
@@ -11,16 +11,14 @@ app.set('views', 'views');
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-
 app.use(bodyParser.urlencoded({extended: false}));
-// Serve Static files
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/admin', adminData.routes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+    res.status(404).render('404', {pageTitle: 'Page Not Found'});
 });
 
 app.listen(3000);
