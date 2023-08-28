dfc41e0c3707dc1e4d9a2f927ec091e9f0d1fbbc
CAMIRO <juancamilosc@hotmail.com>
2023-08-28 10:57:53 -0500

    Displaying product data

--- controllers/admin.js
+++ controllers/admin.js
@@ -1,17 +1,18 @@
 const Product = require('../models/product')
 
-exports.getAddProduct = (req, res, next) => {
-    res.render('admin/add-product', {
-        pageTitle: 'Add Product',
-        path: '/admin/add-product',
-        formsCSS: true,
-        productCSS: true,
-        activeAddProduct: true
-    });
-  };
+  exports.getAddProduct = (req, res, next) => {
+      res.render('admin/add-product', {
+          pageTitle: 'Add Product',
+          path: '/admin/add-product',
+          formsCSS: true,
+          productCSS: true,
+          activeAddProduct: true
+      });
+    };
   
   exports.postAddProduct = (req, res, next) => {
-    const product = new Product(req.body.title)
+    const { title, imageUrl, description, price } = req.body
+    const product = new Product(title, imageUrl, description, price)
     product.save()
     res.redirect('/');
   };
