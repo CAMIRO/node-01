b5144879f4d92346ec0869941135f8075956c6ca
CAMIRO <juancamilosc@hotmail.com>
2023-08-30 11:28:30 -0500

    Editing the Product Data

--- controllers/admin.js
+++ controllers/admin.js
@@ -10,7 +10,7 @@ const Product = require('../models/product')
   
   exports.postAddProduct = (req, res, next) => {
     const { title, imageUrl, description, price } = req.body
-    const product = new Product(title, imageUrl, description, price)
+    const product = new Product(null, title, imageUrl, description, price)
     product.save()
     res.redirect('/');
   };
@@ -37,7 +37,16 @@ const Product = require('../models/product')
   };
 
   exports.postEditProduct = (req, res, next) => {
-
+    const { productId , title, price, imageUrl, description } = req.body
+    const updatedProduct = new Product(
+      productId, 
+      title,
+      imageUrl,
+      description,
+      price
+    )
+    updatedProduct.save()
+    res.redirect('/admin/products')
   }
 
   exports.getProducts = (req, res, next) => {
