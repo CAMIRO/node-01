68d139a81f7a3edf0a9f4b8d2dd94218819bbea9
CAMIRO <juancamilosc@hotmail.com>
2023-08-31 10:19:40 -0500

    Displaying Cart Items on the Cart Page

--- models/cart.js
+++ models/cart.js
@@ -53,4 +53,14 @@ module.exports = class Cart {
         fs.writeFile(p, JSON.stringify(updatedCart), err => console.log(err))
     })
    }
+   static getCart(cb){
+    fs.readFile(p, (err, fileContent) => {
+        const cart = JSON.parse(fileContent)
+        if (err) {
+            cb(null)
+        } else {
+            cb(cart)
+        }
+    })
+   }
 }
