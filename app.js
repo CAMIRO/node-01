629e5014efd04a5b8bbb0d8bd5f609abb0df5842
CAMIRO <juancamilosc@hotmail.com>
2023-09-03 08:30:50 -0500

    Creating and managing a dummy User

--- app.js
+++ app.js
@@ -19,6 +19,15 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
+app.use((req, res, next) =>{
+    User.findByPk(1)
+    .then(user => {
+        req.user = user
+        next()
+    })
+    .catch(err => console.log(err))
+})
+
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
@@ -29,9 +38,20 @@ Product.belongsTo(User, { constraints: true,  onDelete: 'CASCADE' })
 User.hasMany(Product)
 
 sequelize
-    .sync({ force: true })
+    //.sync({ force: true })
+    .sync()
     .then(result => {
-        // console.log(result)
+        return User.findByPk(1)
+    })
+    .then(user =>{
+        if(!user){
+            User.create({ name: 'Mario', email:'mario@test.com' })
+        }
+        return user
+
+    })
+    .then(user => {
+        // console.log(user)
         app.listen(3000)
     })
     .catch(err => console.log(err))
