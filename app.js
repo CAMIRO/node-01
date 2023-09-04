56407b5639c17b4db39ea4c5d9da82e642d9762f
CAMIRO <juancamilosc@hotmail.com>
2023-09-04 10:09:16 -0500

    Adding an Order Model

--- app.js
+++ app.js
@@ -5,10 +5,13 @@ const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error')
 const sequelize = require('./util/database')
+// models
 const Product = require('./models/product')
 const User = require('./models/user')
 const Cart = require('./models/cart')
 const CartItem = require('./models/cart-item')
+const Order = require('./models/order')
+const OrderItem = require('./models/order-item')
 
 const app = express();
 
@@ -41,6 +44,9 @@ User.hasMany(Product)
 User.hasOne(Cart)
 Cart.belongsToMany(Product, { through: CartItem })
 Product.belongsToMany(Cart, { through: CartItem })
+Order.belongsTo(User)
+User.hasMany(Order)
+Order.belongsToMany(Product, { through: OrderItem  })
 
 
 sequelize
