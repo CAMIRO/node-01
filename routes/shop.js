81cae80de2753a420fbd76efd98553f374b04535
CAMIRO <juancamilosc@hotmail.com>
2023-09-05 08:27:45 -0500

    Storing CartItems as OrderItems

--- routes/shop.js
+++ routes/shop.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 const express = require('express');
 
 const shopController = require('../controllers/shop');
@@ -17,6 +15,8 @@ router.get('/cart', shopController.getCart)
 router.post('/cart', shopController.postCart)
 
 router.post('/cart-delete-item', shopController.postCartDelete)
+
+router.post('/create-order', shopController.postOrder)
 
 router.get('/orders', shopController.getOrders)
 
