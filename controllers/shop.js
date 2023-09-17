7210c718e8c9b822ec65223cf87629547374695c
CAMIRO <juancamilosc@hotmail.com>
2023-09-17 09:59:39 -0500

    Fetching a single product

--- controllers/shop.js
+++ controllers/shop.js
@@ -1,7 +1,7 @@
 const Product = require('../models/product')
 
 exports.getProducts = (req, res, next) => {
-  Product.findAll()
+  Product.fetchAll()
     .then(products => {
       res.render('shop/product-list', {
         prods: products,
@@ -14,7 +14,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const { productId } = req.params
-  Product.findByPk(productId)
+  Product.findById(productId)
     .then(product=>{
       res.render('shop/product-detail', { 
         product: product,
