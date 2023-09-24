efd6805dd1588066278cf41309e543f2cde78ff8
CAMIRO <juancamilosc@hotmail.com>
2023-09-24 10:12:56 -0500

    Creating new Users

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require("body-parser");
 
 const errorController = require("./controllers/error");
 const mongoConnect = require("./util/database").mongoConnect
+const User = require('./models/user')
 
 const app = express();
 
@@ -18,6 +19,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
+  User
+    .findById("651051138359e4d477052ee1")
+    .then(user => req.user = user)
+    .catch(err => console.log(err))
   next()
 });
 
