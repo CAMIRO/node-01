a488f9cd3b0c9335bfd659bd7dd0817499042e63
CAMIRO <juancamilosc@hotmail.com>
2023-09-26 09:44:28 -0500

    Deleting Cart Items

--- controllers/shop.js
+++ controllers/shop.js
@@ -56,50 +56,16 @@ exports.postCart = (req, res, next) => {
   Product
     .findById(productId)
     .then(product => req.user.addToCart(product))
-    .then(result => console.log(result))
-
-  // let fetchedCart;
-  // let newQuantity = 1
-  // req.user
-  //   .getCart()
-  //   .then(cart => {
-  //     fetchedCart = cart
-  //     return cart.getProducts({ where: { id: productId } })
-  //   })
-  //   .then(products => {
-  //     let product
-  //     if (products.length > 0) {
-  //       product = products[0]
-  //     }
-  //     if (product) {
-  //       const oldQuantity = product.cartItem.quantity
-  //       newQuantity = oldQuantity + 1
-  //       return product
-  //     }
-  //     return Product.findByPk(productId)
-  //   })
-  //   .then(product => {
-  //     return fetchedCart.addProduct(product, {
-  //       through: { quantity: newQuantity } 
-  //     })
-  //   })
-  //   .then(() => {
-  //     res.redirect('/cart')
-  //   })
-  //   .catch(err => console.log(err))
+    .then(result => {
+      console.log(result)
+      res.redirect('/cart')
+    })
 }
 
 exports.postCartDelete = (req, res, next) => {
   const { productId } = req.body
   req.user
-    .getCart()
-    .then(cart => {
-      return cart.getProducts({ where: { id: productId } })
-    })
-    .then(products => {
-      const product = products[0]
-      return  product.cartItem.destroy()
-    })
+    .deleteItemFromCart(productId)
     .then(result => {
       res.redirect('/cart')
     })
