812fc0fbf0cd570105d9361d194846cbe8c82cf3
CAMIRO <juancamilosc@hotmail.com>
2023-09-28 15:25:54 -0500

    Adding and using a User Model

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ const mongoose = require('mongoose')
 require("dotenv").config();
 
 const errorController = require("./controllers/error");
-// const User = require('./models/user')
+const User = require('./models/user')
 
 const app = express();
 
@@ -19,21 +19,16 @@ const shopRoutes = require("./routes/shop");
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// app.use((req, res, next) => {
-//   User
-//     .findById("651051138359e4d477052ee1")
-//     .then(user => {
-//       req.user = new User(
-//         user.name, 
-//         user.email, 
-//         user.cart, 
-//         user._id
-//       )
-//       next()
-//     })
-//     .catch(err => console.log(err))
+app.use((req, res, next) => {
+  User
+    .findById("6515e109cc5a0165389b85f0")
+    .then(user => {
+      req.user = user
+      next()
+    })
+    .catch(err => console.log(err))
 
-// });
+});
 
 app.use("/admin", adminRoutes);
 app.use(shopRoutes);
@@ -48,5 +43,22 @@ const uri = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_CLUSTER}.mongodb.net/s
 
 mongoose
   .connect(uri)
-  .then(result => app.listen(3000))
+  .then(result => {
+    User
+      .findOne()
+      .then(user => {
+        if (!user) {
+          const user = new User({ 
+            name: 'John',
+            email: 'john@test.com',
+            cart: {
+              items: []
+            }
+           })
+           user.save()
+        }
+      })
+   
+    app.listen(3000)
+  })
   .catch(err => console.log(err))
