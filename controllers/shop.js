6655485482c9f10d68ff37502bfa9352c857bbc0
CAMIRO <juancamilosc@hotmail.com>
2023-09-29 11:41:11 -0500

    Loading the cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -40,8 +40,9 @@ exports.getIndex = (req, res, next) => {
 
 exports.getCart = (req, res, next) => {
   req.user
-    .getCart()
-    .then(products => {
+    .populate('cart.items.productId')
+    .then(user => {
+        const products = user.cart.items
             res.render('shop/cart', {
               path: '/cart',
               pageTitle: 'Your Cart',
@@ -49,7 +50,6 @@ exports.getCart = (req, res, next) => {
           })
     })
     .catch(err => console.log(err))
-
 }
 
 exports.postCart = (req, res, next) => {
