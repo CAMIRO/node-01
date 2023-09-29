709607de712c6c617f1482747aa3b5d1cadf7f3a
CAMIRO <juancamilosc@hotmail.com>
2023-09-29 10:45:44 -0500

    working on the Shopping Cart

--- controllers/admin.js
+++ controllers/admin.js
@@ -85,8 +85,12 @@ const Product = require('../models/product')
   }
 
   exports.getProducts = (req, res, next) => {
-    Product.find()
+    Product
+      .find()
+      // .select('title price -_id')
+      // .populate('userId', 'name')
       .then(products => {
+        console.log(products)
         res.render('admin/products', {
           prods: products,
           pageTitle: 'Admin products',
