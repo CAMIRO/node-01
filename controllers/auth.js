e6ed15eaec303ab3f74fa01f244169cd0672290e
CAMIRO <juancamilosc@hotmail.com>
2023-10-05 16:53:46 -0500

    Setting up bases fro first steps implemneting authentication

--- controllers/auth.js
+++ controllers/auth.js
@@ -8,6 +8,14 @@ exports.getLogin = (req, res, next) => {
       })
 }
 
+exports.getSignup = (req, res, next) => {
+  res.render('auth/signup', {
+      path: '/signup',
+      pageTitle: 'Signup',
+      isAuthenticated: false
+    })
+}
+
 
 exports.postLogin = (req, res, next) => {
   // fetch the user
@@ -24,6 +32,8 @@ exports.postLogin = (req, res, next) => {
   })
   .catch(err => console.log(err))
 }
+
+exports.postSignup = (req, res, next) => {}
   
 exports.postLogout = (req, res, next) => {
  req.session.destroy(err =>{
