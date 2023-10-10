fcfc6d8725f47d291da035e75ec86701d6c15e70
CAMIRO <juancamilosc@hotmail.com>
2023-10-10 11:19:52 -0500

    Styling flash messages and adding them into sign in and singUp views

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,17 +3,31 @@ const bcrypt = require('bcryptjs')
 const User = require('../models/user')
 
 exports.getLogin = (req, res, next) => {
+  let message = req.flash('error')
+  if(message.length > 0 ){
+    message = message[0]
+  } else {
+    message = null
+  }
+
     res.render('auth/login', {
         path: '/login',
         pageTitle: 'Login',
-        errorMessage: req.flash('error')
+        errorMessage: message
       })
 }
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash('error')
+  if(message.length > 0 ){
+    message = message[0]
+  } else {
+    message = null
+  }
   res.render('auth/signup', {
       path: '/signup',
-      pageTitle: 'Signup'
+      pageTitle: 'Signup',
+      errorMessage: message
     })
 }
 
@@ -41,6 +55,7 @@ exports.postLogin = (req, res, next) => {
             res.redirect('/')    
           })  
         }
+        req.flash('error', 'Invalid email or password.')
         res.redirect('/login')
       })
       .catch(err => console.log(err))
@@ -60,6 +75,7 @@ exports.postSignup = (req, res, next) => {
     .findOne({ email })
     .then(userDoc => {
       if(userDoc){
+        req.flash('error', 'Email already registered')
         return res.redirect('/signup')
       }
 
