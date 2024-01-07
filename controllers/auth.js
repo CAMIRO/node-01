1455d4ce25190b089bd7bbf9281b600a772ffd88
juan sarmiento <juan.sarmiento@autodesk.com>
2024-01-07 11:17:13 -0500

    Adding conditional CSS Classes

--- controllers/auth.js
+++ controllers/auth.js
@@ -34,7 +34,8 @@ exports.getSignup = (req, res, next) => {
         email: '', 
         password: '',
         confirmPassword: ''
-      }
+      },
+      validationErrors: []
     })
 }
 
@@ -99,7 +100,8 @@ exports.postSignup = (req, res, next) => {
         email, 
         password,
         confirmPassword: req.body.confirmPassword 
-      }
+      },
+      validationErrors: errors.array()
     })
   }
     bcrypt
